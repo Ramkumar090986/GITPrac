441a483f36523de2152dccb6ff62c32998180173
Ramkumar090986 <ramkumar8687@gmail.com>
2022-11-28 10:57:47 +0530

    Sample Code created

---
+++ src/org/prac/Sample.java
@@ -0,0 +1,15 @@
+package org.prac;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		
+		int a=5,b=10,sum;
+		
+		sum=a+b;
+		
+		System.out.println("Sum of a and b is:"+sum);
+	
+	}
+
+}
